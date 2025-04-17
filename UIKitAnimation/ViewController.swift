b72d6a45cb3587bf46ea10245961f1c7bad06dc8
glt3953 <395313544@qq.com>
2025-04-17 14:32:38 +0800

    初始化相关

---
+++ UIKitAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitAnimation
+//
+//  Created by 宁侠 on 2025/4/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIKitAnimationTests/UIKitAnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitAnimationTests.swift
+//  UIKitAnimationTests
+//
+//  Created by 宁侠 on 2025/4/17.
+//
+
+import Testing
+@testable import UIKitAnimation
+
+struct UIKitAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
